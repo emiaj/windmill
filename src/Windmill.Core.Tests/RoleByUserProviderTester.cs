ed384f6bea2eae035abf2691c5874edb65afedc0
jfebres <jaime.febres@gmail.com>
2013-09-09 16:36:01 -0500

    Smoke.

---
+++ src/Windmill.Core.Tests/RoleByUserProviderTester.cs
@@ -0,0 +1,18 @@
+﻿using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace Windmill.Core.Tests
+{
+    [TestFixture]
+    public class RoleByUserProviderTester : InteractionContext<RoleByUserProvider>
+    {
+        protected override void beforeEach()
+        {
+        }
+
+        [Test]
+        public void smoke()
+        {
+        }
+    }
+}
